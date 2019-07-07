eb2171829351c75313cea0b8fb76ee2c34d38ab7
Julian Giuca <julian@newrelic.com>
2019-07-07 09:22:21 -0700

    this shit doesn't work

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -5,11 +5,13 @@ set -euf -o pipefail
 git clone https://github.com/juliangiuca/logginator.git
 git clone https://github.com/juliangiuca/logginator-db.git
 
-cd logginator-db
-gem install bundler && bundle
-rake db:create
-rake db:migrate
-rake db:seed
+RAILS_ENV=production
+
+cd logginator-db && \
+  gem install bundler && bundle && \
+  rake db:create && \
+  rake db:migrate && \
+  rake db:seed
 
 cd ../logginator
 npm i
