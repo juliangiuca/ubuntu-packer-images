facf2c984f77d1ce481bd535907cffa4bb2c15e7
Julian Giuca <julian@newrelic.com>
2019-07-07 09:33:53 -0700

    *shrugs*

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -4,14 +4,3 @@ set -euf -o pipefail
 
 git clone https://github.com/juliangiuca/logginator.git
 git clone https://github.com/juliangiuca/logginator-db.git
-
-RAILS_ENV=production
-
-cd logginator-db && \
-  gem install bundler && bundle && \
-  rake db:create && \
-  rake db:migrate && \
-  rake db:seed
-
-cd ../logginator
-npm i
